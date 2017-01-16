8006c4c6f37a69f328959ec1ba3d564e9bf7da5e
bgulbis <brian.gulbis@gmail.com>
2017-01-16 12:57:37 -0600

    start basic tidying

--- src/2-tidy_data_for_export.R
+++ src/2-tidy_data_for_export.R
@@ -3,3 +3,19 @@
 library(tidyverse)
 library(edwr)
 
+demographics <- read_data("data/raw", "demographics") %>%
+    as.demographics()
+
+diagnosis <- read_data("data/raw", "diagnosis") %>%
+    as.diagnosis() %>%
+    tidy_data()
+
+keep_labs <- c("anti-xa low molecular heparin", "creatinine lvl")
+
+labs <- read_data("data/raw", "labs") %>%
+    as.labs() %>%
+    tidy_data() %>%
+    filter(lab %in% keep_labs)
+
+measures <- read_data("data/raw", "measures") %>%
+    as.measures()
