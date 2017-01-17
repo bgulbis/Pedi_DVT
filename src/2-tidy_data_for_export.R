b9faea77ee7d97d80820ad3605826ca65303dcc9
bgulbis <brian.gulbis@gmail.com>
2017-01-17 11:40:32 -0600

    update basic tidying

--- src/2-tidy_data_for_export.R
+++ src/2-tidy_data_for_export.R
@@ -15,7 +15,21 @@ keep_labs <- c("anti-xa low molecular heparin", "creatinine lvl")
 labs <- read_data("data/raw", "labs") %>%
     as.labs() %>%
     tidy_data() %>%
-    filter(lab %in% keep_labs)
+    filter(lab %in% keep_labs,
+           !is.na(lab.result))
 
 measures <- read_data("data/raw", "measures") %>%
     as.measures()
+
+meds_freq <- read_data("data/raw", "meds_freq") %>%
+    as.meds_freq()
+
+meds_home <- read_data("data/raw", "meds_home") %>%
+    as.meds_home() %>%
+    filter(med.type == "Recorded / Home Meds")
+
+rad <- read_data("data/raw", "radiology") %>%
+    as.radiology()
+
+id <- read_data("data/raw", "identifiers") %>%
+    as.id()
